54493874ee78c76df2d0afb868d9d1c347da1ff4
Sourav Dubey <souravdubey754@gmail.com>
2024-11-29 04:40:28 +0530

    add password hash

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { NextFunction ,Response , Request} from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
 
 const createUser = async (req :Request , res :Response ,next :NextFunction)=>{
 
@@ -17,6 +18,7 @@ const createUser = async (req :Request , res :Response ,next :NextFunction)=>{
         return next(error);
     }
     
+    const hasPassword = await bcrypt.hash(password , 10); 
     
  
 
