d0c3ec856ff64836b294d5bd53a12e495ff82d48
Sourav Dubey <souravdubey754@gmail.com>
2024-12-03 08:13:47 +0530

    add error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import {User} from "./userType";
 
 const createUser = async (req :Request , res :Response ,next :NextFunction)=>{
 
@@ -12,23 +13,41 @@ const createUser = async (req :Request , res :Response ,next :NextFunction)=>{
        const error =createHttpError(400 ,"all feilds are required!"); 
        return next(error);
     }
-    
-    // Check if email already exists
-    const user = await userModel.findOne({ email });
-    if(user){
-        const error = createHttpError(400 ,"email already exists!");
-        return next(error);
+
+    try{
+        const user = await userModel.findOne({ email });
+        if(user){
+            const error = createHttpError(400 ,"email already exists!");
+            return next(error);
+        }
+        
+    }catch(error){
+        return next(createHttpError(500, `${error.message}`));
     }
     
+
+   
+   
     const hashPassword = await bcrypt.hash(password , 10); 
-
-    const newUser = await new userModel({
-        name,
-        email,
-        password: hashPassword
-    });
-    const token = sign({sub: newUser._id,} ,config.jwtSecret as string , {expiresIn: '7d'}); 
-
+    let newUser = User;
+    try{
+         newUser = await new userModel({
+            name,
+            email,
+            password: hashPassword
+        });
+    }catch(error){
+        return next(createHttpError(500, "Error creating user"));}
+   
+        try{
+            const token = sign({sub: newUser._id,} ,config.jwtSecret as string , {expiresIn: '7d'}); 
+            res.json({
+                accessToken :token,
+             })
+        }catch(error){
+            return next(createHttpError(500, "Error creating token"));
+        }
+    
     
  
 
@@ -38,9 +57,7 @@ const createUser = async (req :Request , res :Response ,next :NextFunction)=>{
 
 
 
-    res.json({
-       accessToken :token,
-    })
+   
 
 }
 
